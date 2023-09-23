97ef80895b3d2ba178b5087bce50ac8b2c0cd3f2
Gianka <giankalinares98@gmail.com>
2023-09-23 12:39:22 -0600

    Arreglo de footer

--- src/componentes/Footeer/index.jsx
+++ src/componentes/Footeer/index.jsx
@@ -1,7 +1,8 @@
 import "./Footer.css";
+import footerBg from "../../../public/img/footer.png"
 
 const Footer = ()=>{
-    return <footer className="footer" style={{backgroundImage:"url(/img/Footer.png)"}}>
+    return <footer className="footer" style={{backgroundImage:`url(${footerBg})`}}>
         <div className="redes">
         <a href='https://www.aluracursos.com/'> 
             <img src="/img/facebook.png" alt="Facebook"/>
@@ -14,7 +15,7 @@ const Footer = ()=>{
         </a>
         </div>
         <img src='/img/Logo.png' alt='org' /> 
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado por Giankarlow Linares</strong>
     </footer>
 }
 export default Footer;
